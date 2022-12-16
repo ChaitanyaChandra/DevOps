d5e2833a0bf907d47841558ed12a2d89059a07f9
chaitanya chandra <chay@outlook.in>
2022-12-16 15:05:53 +0530

    automated commit #1402

---
+++ 1.LINUX/KeyGen/code.sh
@@ -0,0 +1,10 @@
+# config file stored in ~/.ssh/config
+Host xxx
+    HostName 10.XXX
+    User XXX
+    Port XXX
+    IdentityFile ~/.ssh/XXX
+    StrictHostKeyChecking no
+
+# permissions required for config file
+chmod 600 ~/.ssh/config
