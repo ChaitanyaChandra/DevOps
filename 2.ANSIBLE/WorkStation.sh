94b51241123318fe8f1bcdf2b2465073ef82f6b9
chaitanya chandra <chay@outlook.in>
2023-01-09 10:56:00 +0530

    automated commit #1481

---
+++ 2.ANSIBLE/WorkStation.sh
@@ -0,0 +1,6 @@
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
+sudo -y yum install git python3
+python3 -m pip install ansible
+
