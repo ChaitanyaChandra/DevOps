d744379802adc13a407e6bbcdd1b9764c2141c98
chaitanya chandra <chay@outlook.in>
2023-01-09 15:37:56 +0530

    automated commit #1500

--- 2.ANSIBLE/WorkStation.sh
+++ 2.ANSIBLE/WorkStation.sh
@@ -1,6 +1,5 @@
 sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
-sudo -y yum install git python3
-python3 -m pip install ansible
-
+sudo yum install python39-devel -y
+pip3.9 install ansible botocore boto3
--- 2.ANSIBLE/run.sh
+++ 2.ANSIBLE/run.sh
@@ -1,1 +1,1 @@
-ansible-playbook spec.yml -e 'ansible_python_interpreter=/usr/bin/python3' -e 'ROLE=datadog'
+ansible-playbook spec.yml  -e 'ROLE=datadog'
