f6a1abc742035e9e33fe27782bf08d776cef53c2
chaitanya chandra <chay@outlook.in>
2023-01-12 17:54:22 +0530

    automated commit #1547

--- 2.ANSIBLE/WorkStation.sh
+++ 2.ANSIBLE/WorkStation.sh
@@ -2,4 +2,4 @@ sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
 sudo yum install python39-devel  git -y
-pip3.9 install ansible botocore boto3
+sudo pip3.9 install ansible botocore boto3
