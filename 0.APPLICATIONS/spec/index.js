a1505dc5c30489276106e39fd2ce3bf93d0ffa91
chaitanya chandra <chay@outlook.in>
2023-01-23 14:10:23 +0530

    automated commit #1589

--- 0.APPLICATIONS/spec/index.js
+++ 0.APPLICATIONS/spec/index.js
@@ -9,10 +9,8 @@ let password = "@123Chaitu"
 
 const app = express()
 const port = 8080
-const ver = "0"
-const pre = "0"
-const preversion = process.env.VERSION || pre
-const version = ver + "." + preversion
+const ver = "0.0"
+const version = process.env.VERSION || pre
 
 app.use(express.static('public'))
 app.use(express.static('views/public'))
