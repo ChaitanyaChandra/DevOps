373ce5598486b36532a75c34e515187f80aafabe
chaitanya chandra <chay@outlook.in>
2023-02-06 15:56:05 +0530

    automated commit #1725

--- 3.JENKINS/vars/java.groovy
+++ 3.JENKINS/vars/java.groovy
@@ -8,8 +8,8 @@ def call() {
         sh 'mvn compile'
       }
       common.codeQuality()
-      common.codeChecks()
-      common.artifacts()
+      // common.codeChecks()
+      // common.artifacts()
     } catch (Exception e) {
       mail bcc: '', body: "Build Failed ${RUN_DISPLAY_URL}", cc: '', from: 'raghuk.vit@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'raghuk.vit@gmail.com'
     }
