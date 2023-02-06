2eab52703e5f4a8a63c1fa98105c350a54eabba1
chaitanya chandra <chay@outlook.in>
2023-02-06 19:12:10 +0530

    automated commit #1750

--- 3.JENKINS/vars/common.groovy
+++ 3.JENKINS/vars/common.groovy
@@ -47,8 +47,7 @@ def codeQuality() {
     sh """
     git remote set-url origin https://${gitUser}:${gitPass}@github.com/chaitanyachandra/${COMPONENT}.git
     # delete branch locally
-    git checkout ${env.BRANCH_NAME}
-    git branch -d ${env.BRANCH_NAME}
+    # git branch -d ${env.BRANCH_NAME}
 
     # delete branch remotely
     git push origin --delete ${env.BRANCH_NAME}
