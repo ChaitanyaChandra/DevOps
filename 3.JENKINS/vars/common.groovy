6757376ad8284aa6589893909b4ab4980df7fcc6
chaitanya chandra <chay@outlook.in>
2023-02-06 19:20:03 +0530

    automated commit #1752

--- 3.JENKINS/vars/common.groovy
+++ 3.JENKINS/vars/common.groovy
@@ -19,6 +19,7 @@ def codeQuality() {
     stage('merge TO main') {
     withCredentials([usernamePassword(credentialsId: 'GIT_CREDS', passwordVariable: 'gitPass', usernameVariable: 'gitUser')]){
     sh """
+    echo ${env}
     # git remote add origin https://${gitUser}:${gitPass}@github.com/chaitanyachandra/${COMPONENT}.git
 
     git remote set-url origin https://${gitUser}:${gitPass}@github.com/chaitanyachandra/${COMPONENT}.git
