6a20a6b85efca47d0d1d415c61afcd500882d859
chaitanya chandra <chay@outlook.in>
2023-02-10 18:02:15 +0530

    automated commit #1802

--- 4.Terraform/common/BastionHost&applications/r53.tf
+++ 4.Terraform/common/BastionHost&applications/r53.tf
@@ -8,14 +8,14 @@ resource "aws_route53_zone" "dns" {
   }), local.tags)
 }
 
-#resource "aws_route53_record" "route" {
-#  count   = length(local.instance_types)
-#  zone_id = aws_route53_zone.dns.zone_id
-#  name    = "${local.instance_types[count.index]}.${var.domain}"
-#  type    = "A"
-#  ttl     = 60
-#  records =
-#}
+resource "aws_route53_record" "route" {
+  count   = length(local.instance_types)
+  zone_id = aws_route53_zone.dns.zone_id
+  name    = "${local.instance_types[count.index]}.${var.domain}"
+  type    = "A"
+  ttl     = 60
+  records = aws_spot_instance_request.vm.*.public_ip[count.index]
+}
 
 output "value" {
   value = aws_spot_instance_request.vm.*.public_ip
