a9f57a5f4e9ffcfb6196a52f95478cd80a1d6487
chaitanya chandra <chay@outlook.in>
2023-02-13 15:51:58 +0530

    automated commit #1921

--- 4.Terraform/mutable/main.tf
+++ 4.Terraform/mutable/main.tf
@@ -1,9 +1,19 @@
-module "asg" {
+module "nodejs" {
   source = "github.com/chaitanyachandra/devops/4.Terraform/modules/mutable/asg"
   min_size = var.min_size
   max_size = var.max_size
   app_port_no = var.app_port_no
-  component_role = var.component_role
+  component_role = "backend_nodejs"
+  APP_TYPE = var.APP_TYPE
+  ENV = var.ENV
+}
+
+module "nginx" {
+  source = "github.com/chaitanyachandra/devops/4.Terraform/modules/mutable/asg"
+  min_size = var.min_size
+  max_size = var.max_size
+  app_port_no = var.app_port_no
+  component_role = "frontend"
   APP_TYPE = var.APP_TYPE
   ENV = var.ENV
 }
--- 4.Terraform/mutable/vars.tf
+++ 4.Terraform/mutable/vars.tf
@@ -1,6 +1,5 @@
 variable "min_size" {}
 variable "max_size" {}
 variable "app_port_no" {}
-variable "component_role" {}
 variable "APP_TYPE" {}
 variable "ENV" {}
