31e445b4afac343923ec8061362bcef86c23b4f8
chaitanya chandra <chay@outlook.in>
2023-02-13 09:07:06 +0530

    automated commit #1891

--- 4.Terraform/modules/common/vpc/main.tf
+++ 4.Terraform/modules/common/vpc/main.tf
@@ -38,42 +38,42 @@ resource "aws_subnet" "public_subnet" {
     "Name" = "${local.tags.Service}-${local.Environment}-${local.env_tag.appenv}-public-subnet-${count.index}"
   }), local.tags)
 }
-#
-#
-## Creating RT for Public Subnet one
-#resource "aws_route_table" "public_route_table" {
-#  vpc_id = aws_vpc.vpc.id
-#  route {
-#    cidr_block = "0.0.0.0/0" # Traffic from Public Subnet reaches Internet via Internet Gateway
-#    gateway_id = aws_internet_gateway.IGW.id
-#  }
-#  route {
-#    cidr_block         = "172.16.0.0/21"
-#    transit_gateway_id = data.terraform_remote_state.remote.outputs.tgw_id
-#  }
-#  tags = merge(tomap({
-#    "Name" = "${local.tags.Service}-${local.Environment}-${local.env_tag.appenv}-public-route-table"
-#  }), local.tags)
-#  depends_on = [aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_one]
-#}
 
-## Route table Association with Public Subnet one
-#resource "aws_route_table_association" "PublicRT_association" {
-#  count          = length(var.PUBLIC_SUBNET_CIDR)
-#  subnet_id      = aws_subnet.public_subnet[count.index].id
-#  route_table_id = aws_route_table.public_route_table.id
-#}
-#
-## private subnet in az-a
-#resource "aws_subnet" "private_subnet" {
-#  count             = length(var.PRIVATE_SUBNET_CIDR)
-#  vpc_id            = aws_vpc.vpc.id
-#  cidr_block        = var.PRIVATE_SUBNET_CIDR[count.index]
-#  availability_zone = data.aws_availability_zones.az.names[count.index]
-#  tags = merge(tomap({
-#    "Name" = "${local.tags.Service}-${local.Environment}-${local.env_tag.appenv}-private-subnet-${count.index}"
-#  }), local.tags)
-#}
+
+# Creating RT for Public Subnet one
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0" # Traffic from Public Subnet reaches Internet via Internet Gateway
+    gateway_id = aws_internet_gateway.IGW.id
+  }
+  route {
+    cidr_block         = "172.16.0.0/21"
+    transit_gateway_id = data.terraform_remote_state.remote.outputs.tgw_id
+  }
+  tags = merge(tomap({
+    "Name" = "${local.tags.Service}-${local.Environment}-${local.env_tag.appenv}-public-route-table"
+  }), local.tags)
+  depends_on = [aws_ec2_transit_gateway_vpc_attachment.tgw_attachment_one]
+}
+
+# Route table Association with Public Subnet one
+resource "aws_route_table_association" "PublicRT_association" {
+  count          = length(var.PUBLIC_SUBNET_CIDR)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# private subnet in az-a
+resource "aws_subnet" "private_subnet" {
+  count             = length(var.PRIVATE_SUBNET_CIDR)
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.PRIVATE_SUBNET_CIDR[count.index]
+  availability_zone = data.aws_availability_zones.az.names[count.index]
+  tags = merge(tomap({
+    "Name" = "${local.tags.Service}-${local.Environment}-${local.env_tag.appenv}-private-subnet-${count.index}"
+  }), local.tags)
+}
 #
 ## Creating RT for Private Subnet one
 #resource "aws_route_table" "private_route_table_one" {
--- 4.Terraform/modules/common/vpc/tgw.tf
+++ 4.Terraform/modules/common/vpc/tgw.tf
@@ -1,8 +1,8 @@
-#resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_one" {
-#  subnet_ids         = [aws_subnet.private_subnet[0].id, aws_subnet.private_subnet[1].id]
-#  transit_gateway_id = data.terraform_remote_state.remote.outputs.tgw_id
-#  vpc_id             = aws_vpc.vpc.id
-#  tags = merge(tomap({
-#    "Name" = "${local.tags.Service}-${local.Environment}-tgw-internal-attachment"
-#  }), local.tags)
-#}
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment_one" {
+  subnet_ids         = [aws_subnet.private_subnet[0].id, aws_subnet.private_subnet[1].id]
+  transit_gateway_id = data.terraform_remote_state.remote.outputs.tgw_id
+  vpc_id             = aws_vpc.vpc.id
+  tags = merge(tomap({
+    "Name" = "${local.tags.Service}-${local.Environment}-tgw-internal-attachment"
+  }), local.tags)
+}
