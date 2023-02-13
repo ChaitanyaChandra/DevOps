a5a7564602ab920c7c67955e4cb8bd3f6f81d7b9
chaitanya chandra <chay@outlook.in>
2023-02-13 11:17:52 +0530

    automated commit #1903

--- 4.Terraform/common/vpc/vpc.tf
+++ 4.Terraform/common/vpc/vpc.tf
@@ -2,6 +2,6 @@ module "vpc" {
   source = "github.com/chaitanyachandra/devops/4.Terraform/modules/common/vpc"
   VPC_CIDR = var.VPC_CIDR
   ENV = var.ENV
-  PUBLIC_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+  PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
   PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
 }
