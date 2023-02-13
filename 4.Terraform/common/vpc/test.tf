66b1938e3a532bac3f8ebda3106d47f4fd29fe23
chaitanya chandra <chay@outlook.in>
2023-02-13 10:57:17 +0530

    automated commit #1900

--- 4.Terraform/common/vpc/test.tf
+++ 4.Terraform/common/vpc/test.tf
@@ -1,88 +1,31 @@
-# Configure the AWS provider
-provider "aws" {
-  region = "us-east-1"
+data "aws_availability_zones" "az" {
+  state = "available"
 }
 
-# Create the VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.VPC_CIDR
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
-# Create the public subnet
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.example.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "public"
-  }
-}
 
-# Create the private subnet
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.example.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "private"
-  }
-}
 
-# Create the internet gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.example.id
-}
-
-# Create the NAT gateway
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public.id
-}
-
-# Create the elastic IP address for the NAT gateway
-resource "aws_eip" "nat" {
-  vpc = true
-}
-
-# Create the route tables
-resource "aws_route_table" "public" {
+# public subnet az-a
+resource "aws_subnet" "public_subnet" {
   count                   = length(var.PUBLIC_SUBNET_CIDR)
-  vpc_id                  = aws_vpc.example.id
+  vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.PUBLIC_SUBNET_CIDR[count.index]
   map_public_ip_on_launch = true
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "public"
-  }
+  availability_zone       = data.aws_availability_zones.az.names[count.index]
 }
 
 
-resource "aws_route_table" "private" {
-  count                   = length(var.PRIVATE_SUBNET_CIDR)
-  vpc_id                  = aws_vpc.example.id
-  cidr_block              = var.PRIVATE_SUBNET_CIDR[count.index]
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat.id
-  }
-
-  tags = {
-    Name = "private"
-  }
+# private subnet in az-a
+resource "aws_subnet" "private_subnet" {
+  count             = length(var.PRIVATE_SUBNET_CIDR)
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.PRIVATE_SUBNET_CIDR[count.index]
+  availability_zone = data.aws_availability_zones.az.names[count.index]
+  depends_on = [aws_subnet.public_subnet]
 }
-
-## Associate the subnets with the route tables
-#resource "aws_route_table_association" "public" {
-#  subnet_id      = aws_subnet.public.id
-#  route_table_id = aws_route_table.public.id
-#}
-#
-#resource "aws_route_table_association" "private" {
-#  subnet_id      = aws_subnet.private.id
-#  route_table_id = aws_route_table.private.id
-#}
