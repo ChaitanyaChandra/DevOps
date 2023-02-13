d54dc76e2be4e9d4ff4fd6162f2c378cb23b5f6f
chaitanya chandra <chay@outlook.in>
2023-02-13 13:31:56 +0530

    automated commit #1909

--- 4.Terraform/modules/common/vpc/outputs.tf
+++ 4.Terraform/modules/common/vpc/outputs.tf
@@ -3,11 +3,11 @@ output "vpc_id" {
 }
 
 output "private_subnets" {
-  value = aws_subnet.private_subnet
+  value = aws_subnet.private_subnet.*.id
 }
 
 output "public_subnets" {
-  value = aws_subnet.public_subnet
+  value = aws_subnet.public_subnet.*.id
 }
 
 output "vpc_cidr" {
