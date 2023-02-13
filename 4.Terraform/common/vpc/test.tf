0d71982c19b525441f24b50f3981b7308b8e894f
chaitanya chandra <chay@outlook.in>
2023-02-13 11:04:45 +0530

    automated commit #1901

--- 4.Terraform/common/vpc/test.tf
+++ 4.Terraform/common/vpc/test.tf
@@ -1,31 +1,31 @@
-data "aws_availability_zones" "az" {
-  state = "available"
-}
-
-
-resource "aws_vpc" "vpc" {
-  cidr_block           = var.VPC_CIDR
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-}
-
-
-
-# public subnet az-a
-resource "aws_subnet" "public_subnet" {
-  count                   = length(var.PUBLIC_SUBNET_CIDR)
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.PUBLIC_SUBNET_CIDR[count.index]
-  map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.az.names[count.index]
-}
-
-
-# private subnet in az-a
-resource "aws_subnet" "private_subnet" {
-  count             = length(var.PRIVATE_SUBNET_CIDR)
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = var.PRIVATE_SUBNET_CIDR[count.index]
-  availability_zone = data.aws_availability_zones.az.names[count.index]
-  depends_on = [aws_subnet.public_subnet]
-}
+#data "aws_availability_zones" "az" {
+#  state = "available"
+#}
+#
+#
+#resource "aws_vpc" "vpc" {
+#  cidr_block           = var.VPC_CIDR
+#  enable_dns_hostnames = true
+#  enable_dns_support   = true
+#}
+#
+#
+#
+## public subnet az-a
+#resource "aws_subnet" "public_subnet" {
+#  count                   = length(var.PUBLIC_SUBNET_CIDR)
+#  vpc_id                  = aws_vpc.vpc.id
+#  cidr_block              = var.PUBLIC_SUBNET_CIDR[count.index]
+#  map_public_ip_on_launch = true
+#  availability_zone       = data.aws_availability_zones.az.names[count.index]
+#}
+#
+#
+## private subnet in az-a
+#resource "aws_subnet" "private_subnet" {
+#  count             = length(var.PRIVATE_SUBNET_CIDR)
+#  vpc_id            = aws_vpc.vpc.id
+#  cidr_block        = var.PRIVATE_SUBNET_CIDR[count.index]
+#  availability_zone = data.aws_availability_zones.az.names[count.index]
+#  depends_on = [aws_subnet.public_subnet]
+#}
--- 4.Terraform/common/vpc/vpc.tf
+++ 4.Terraform/common/vpc/vpc.tf
@@ -1,7 +1,7 @@
-#module "vpc" {
-#  source = "github.com/chaitanyachandra/devops/4.Terraform/modules/common/vpc"
-#  VPC_CIDR = var.VPC_CIDR
-#  ENV = var.ENV
-#  PUBLIC_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-#  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-#}
+module "vpc" {
+  source = "github.com/chaitanyachandra/devops/4.Terraform/modules/common/vpc"
+  VPC_CIDR = var.VPC_CIDR
+  ENV = var.ENV
+  PUBLIC_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+}
