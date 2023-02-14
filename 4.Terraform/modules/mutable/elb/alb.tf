a477ee09fb5f695e58200f0283097083a31898d0
chaitanya chandra <chay@outlook.in>
2023-02-14 12:25:32 +0530

    automated commit #1963

--- 4.Terraform/modules/mutable/elb/alb.tf
+++ 4.Terraform/modules/mutable/elb/alb.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "main" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = var.internal ? [data.terraform_remote_state.remote_vpc.outputs.vpc_id] : ["0.0.0.0/0"]
+    cidr_blocks = var.internal ? [data.terraform_remote_state.remote_vpc.outputs.vpc_cidr] : ["0.0.0.0/0"]
   }
 
   ingress {
@@ -28,7 +28,7 @@ resource "aws_security_group" "main" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = var.internal ? [data.terraform_remote_state.remote_vpc.outputs.vpc_id] : ["0.0.0.0/0"]
+    cidr_blocks = var.internal ? [data.terraform_remote_state.remote_vpc.outputs.vpc_cidr] : ["0.0.0.0/0"]
   }
 
   egress {
